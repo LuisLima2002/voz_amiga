0927d3337b7448d18d996a1111ef5b3c9e16b7f6
Otávio Negrizolli <otavionegrizolli@hotmail.com>
2024-09-25 01:33:55 -0300

    feat(Exercise): add dto do relacionamento das atividades com o exercicio

--- lib/dto/exercise.dto.dart
+++ lib/dto/exercise.dto.dart
@@ -1,9 +1,18 @@
+class ExerciseActivity {
+  final String id;
+  final String name;
+
+  ExerciseActivity.fromJSON(Map<String, dynamic> data)
+      : id = data['id'] ?? '',
+        name = data['name'] ?? '';
+}
+
 class Exercise {
   final String id;
   final String title;
   final String description;
   final int points;
-  final List<String>? activities;
+  final List<ExerciseActivity>? activities;
 
   const Exercise({
     required this.id,
@@ -17,8 +26,15 @@ class Exercise {
       : id = data['id'] ?? '',
         title = data['title'] ?? '',
         description = data['description'] ?? '',
-        activities = data['activities'] ?? [],
+        activities = _handleList(data['activities']),
         points = data['points'] ?? 0;
+
+  static _handleList(dynamic data) {
+    if (data == null || data is! List) return null;
+
+    return data.map((a) => ExerciseActivity.fromJSON(a)).toList();
+  }
+
   @override
   String toString() {
     return '[${super.toString()}]: $id, $title, $points';
